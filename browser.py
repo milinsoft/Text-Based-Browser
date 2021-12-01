6e884669059e83803af2f2208b8b0020f610fb04
parseltongist <>
2021-12-01 13:30:47 +0100

    Stage 4/6 initial commit

--- browser.py
+++ browser.py
@@ -1,85 +1,52 @@
 import os
 import sys
-from os import path
 
 import re
-
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
+import requests
+from bs4 import BeautifulSoup
 
 # write your code here
 
 
 def mkdir(directory, url, text):
-    print("URL is:", url)
     filename = re.search(r"(\w+)\.", url).group()[:-1]
 
-    print("filename is: ", filename)
-
-    #current_dir = os.getcwd()
-    #print(current_dir)
-
-    if path.exists(directory):
-        try:
-            os.mkdir(directory)
-            os.chdir(os.path(directory))
-            print(os.getcwd())
-        except FileExistsError:
-            pass
-        else:
-            with open(filename, "w") as file:
-                file.write(text)
-
+    try:
+        os.mkdir(directory)
+    except FileExistsError:
+        print("FAILURE")
+    else:
+        os.chdir(directory)
+        with open(filename, "w", encoding="UTF-8") as file:
+            file.write(text)
+        print("SUCCESS")
 
 
 def main():
-    directory = sys.argv[0]
+    directory = sys.argv[1]  # 1, not 0
+
+    pages_history = []
 
     while True:
         url = input()
-        if url == "bloomberg.com":
-            # print(bloomberg_com)
-            mkdir(directory, url, bloomberg_com)
-
-        elif url == "nytimes.com":
-            # print(nytimes_com)
-            mkdir(directory, url, nytimes_com)
+        if url not in {"exit", "back"}:
+            r = requests.get(url) if url.startswith("http://") else requests.get("".join(("http://", url)))
+            #print(r.status_code)
+            if r.status_code != 200:
+                print("ERROR!")
+                continue
+            else:
+                soup = BeautifulSoup(r.content, 'html.parser')
+                print(soup.prettify())
+                #print(r.content)
+                mkdir(directory, url, soup.prettify())
+                pages_history.append(soup.prettify())
         elif url == "exit":
             break
-        else:
-            print("ERROR!")
+        elif url == "back":
+            if pages_history:
+                pages_history.pop()
+                print()
 
 
 if __name__ == "__main__":
