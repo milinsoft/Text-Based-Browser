45b7c9d7075f1c37a14b6e30f68c256a1a41c436
parseltongist <>
2021-12-03 01:27:46 +0100

    Stage 4/6 working

--- browser.py
+++ browser.py
@@ -3,23 +3,20 @@ import sys
 
 import re
 import requests
-from bs4 import BeautifulSoup
 
 # write your code here
 
 
 def mkdir(directory, url, text):
-    filename = re.search(r"(\w+)\.", url).group()[:-1]
-
+    filename = re.search(r"(\w+\.)+", url).group()[:-1]
     try:
         os.mkdir(directory)
     except FileExistsError:
-        print("FAILURE")
-    else:
-        os.chdir(directory)
-        with open(filename, "w", encoding="UTF-8") as file:
-            file.write(text)
-        print("SUCCESS")
+        pass
+    os.chdir(directory)
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
+    print("\n file successfully saved")
 
 
 def main():
@@ -31,16 +28,13 @@ def main():
         url = input()
         if url not in {"exit", "back"}:
             r = requests.get(url) if url.startswith("http://") else requests.get("".join(("http://", url)))
-            #print(r.status_code)
             if r.status_code != 200:
                 print("ERROR!")
                 continue
             else:
-                soup = BeautifulSoup(r.content, 'html.parser')
-                print(soup.prettify())
-                #print(r.content)
-                mkdir(directory, url, soup.prettify())
-                pages_history.append(soup.prettify())
+                print(r.text)
+                mkdir(directory, url, r.text)
+                pages_history.append(r.text)
         elif url == "exit":
             break
         elif url == "back":
