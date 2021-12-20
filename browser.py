17d2babaef1e2dc1f86eb235917167392147c828
parseltongist <>
2021-12-20 21:49:39 +0100

    Stage 6/6 final version

--- browser.py
+++ browser.py
@@ -1,50 +1,67 @@
 import os
 import sys
-
 import re
 import requests
 from bs4 import BeautifulSoup
-# write your code here
+import colorama
+from colorama import Fore
 
 
-def mkdir(directory, url, text):
-    filename = re.search(r"(\w+\.)+", url).group()[:-1]
-    try:
-        os.mkdir(directory)
-    except FileExistsError:
-        pass
-    os.chdir(directory)
-    with open(filename, "w", encoding="utf-8") as file:
-        file.write(text)
-    print("\n file successfully saved")
+class TextBasedBrowser:
 
+    def __init__(self, directory: str):
+        self.directory = directory
+        self.cache = []
 
-def main():
-    directory = sys.argv[1]  # 1, not 0
+    def cache_page(self, url: str, content: str):
+        filename = re.search(r"(\w+\.)+", url).group()[:-1]
+        try:
+            os.mkdir(self.directory)
+        except FileExistsError:
+            pass
+        os.chdir(self.directory)
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(content)
+        print("\n file successfully saved")
 
-    pages_history = []
+    def visit_page(self, url: str):
+        try:
+            r = requests.get(url) if url.startswith("http://") else requests.get("".join(("http://", url)))
+        except requests.exceptions.ConnectionError:
+            print("Incorrect URL")
+        else:
+            if r.status_code != 200:
+                print("Connection ERROR!")
+            else:
+                tags = ['p', 'a', 'ul', 'ol', 'li', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+                soup = BeautifulSoup(r.text,
+                                     "html.parser").body.descendants  # zooming in on a body part of the "tree" using .body method,
+                # + visit all children (descendants) of body tag using .descendants mehtod
+                page = ""
+                for descendant in soup:
+                    if descendant.name in tags:
+                        page += Fore.BLUE + descendant.get_text().strip() if descendant.name == "a" else descendant.get_text().strip()
+                print(page)
+                self.cache_page(url, page)
+                self.cache.append(page)
 
-    while True:
-        url = input()
-        if url not in {"exit", "back"}:
-            try:
-                r = requests.get(url) if url.startswith("http://") else requests.get("".join(("http://", url)))
-                if r.status_code != 200:
-                    print("Connection ERROR!")
-                    continue
-                soup = BeautifulSoup(r.content, "html.parser")
-                print(soup.get_text())
-                mkdir(directory, url, soup.get_text())
-                pages_history.append(soup.get_text())
-            except requests.exceptions.ConnectionError:
-                print("Incorrect URL")
-        elif url == "exit":
-            break
-        elif url == "back":
-            if pages_history:
-                pages_history.pop()
-                print()
+    def show_previous_page(self):
+        if len(self.cache) > 1:
+            self.cache.pop()
+            print(self.cache.pop())
+
+    def run_browser(self):
+        while True:
+            url = input()
+            if url == "back":
+                self.show_previous_page()
+            elif url == "exit":
+                break
+            else:
+                self.visit_page(url)
 
 
 if __name__ == "__main__":
-    main()
+    colorama.init(autoreset=True)
+    browser = TextBasedBrowser(sys.argv[1])  # index 1 as 0 corresponds to the python script file.
+    browser.run_browser()
